1e958688c2abd3e204c4dc2663e900b57a628e41
Jaisson <097319@aluno.uricer.edu.br>
2021-12-12 22:28:59 -0300

    Adição da quest 1933

--- 1 - INICIANTE/1930.cpp
+++ 1 - INICIANTE/1930.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
 
     int T1, T2, T3, T4, maximoAparelhos;
+
     cin >> T1 >> T2 >> T3 >> T4;
+
     maximoAparelhos = T1 + T2 + T3 + T4 - 3;
     cout << maximoAparelhos << endl;
 
---
+++ 1 - INICIANTE/1933.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int card1, card2, card3;
+
+    cin >> card1 >> card2;
+
+    if (card1 > card2)
+        cout << card1 << endl;
+    else cout << card2 << endl;
+
+    return 0;
+}
