3f73dd5d321f22883f1ffea2f4a877b9f7697610
Jaisson <097319@aluno.uricer.edu.br>
2021-12-12 22:16:16 -0300

    Adição da quest 1930

---
+++ 1 - INICIANTE/1930.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int T1, T2, T3, T4, maximoAparelhos;
+    cin >> T1 >> T2 >> T3 >> T4;
+    maximoAparelhos = T1 + T2 + T3 + T4 - 3;
+    cout << maximoAparelhos << endl;
+
+    return 0;
+}
