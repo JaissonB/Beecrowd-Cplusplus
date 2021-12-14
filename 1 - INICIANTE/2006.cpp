7146bfb76297876d7cd64fa9013ee117430e7a35
Jaisson <097319@aluno.uricer.edu.br>
2021-12-14 14:20:13 -0300

    Finalizando a quest 2006

--- 1 - INICIANTE/2006.cpp
+++ 1 - INICIANTE/2006.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main() {
 
-    int chaCorreto, palpite, cont;
+    int chaCorreto, palpite, cont = 0;
+    int consumidores = 5;
 
     cin >> chaCorreto;
 
-    while (true) {
-
+    while (consumidores) {
+        cin >> palpite;
+        if (palpite == chaCorreto) cont++;
+        consumidores--;
     }
+    cout << cont << endl;
 
     return 0;
 }
