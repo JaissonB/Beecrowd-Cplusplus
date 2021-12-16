5021949a4043556dbd63aa8c81848bc3429fd885
Jaisson <097319@aluno.uricer.edu.br>
2021-12-16 20:21:49 -0300

    Adição da quest 1864

--- 1 - INICIANTE/1864.cpp
+++ 1 - INICIANTE/1864.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-    string frase = "LIFE IS NOT A PROBLEM TO BESOLVED";
+    string frase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
     int quantidadeCaracteres;
 
     cin >> quantidadeCaracteres;
