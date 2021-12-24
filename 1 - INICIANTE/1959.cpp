0049f651a67e442495354c7c4e7ab613084627fb
Jaisson <097319@aluno.uricer.edu.br>
2021-12-24 16:32:17 -0300

    Adição da quest 1959

---
+++ 1 - INICIANTE/1959.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    long long int lados, comprimento;
+
+    cin >> lados >> comprimento;
+
+    cout << lados * comprimento << endl;
+
+    return 0;
+}
