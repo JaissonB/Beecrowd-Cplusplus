cdd55502c1cb936004c1dc9532e015638c404b8d
JaissonB <097319@aluno.uricer.edu.br>
2022-03-30 14:51:55 -0300

    Adição da quest 2165

---
+++ 1 - INICIANTE/2165.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+    string text;
+
+    getline(cin, text);
+
+    if (text.size() <= 140) cout << "TWEET" << endl;
+    else cout << "MUTE" << endl;
+
+    return 0;
+}
